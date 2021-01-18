ee16ed63d2e17d0c2e704bde61ca1513322349f5
Emily Patsko <Emily.Patsko@softwire.com>
2021-01-18 15:30:56 +0000

    Changed code to print Fizz whenever the number is a multiple of 3 instead of the number itself

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,7 +1,11 @@
 function FizzBuzz() {
     var i;
     for (i = 1; i < 101; i++) {
-        console.log(i);
+        if (i%3 == 0) {
+            console.log("Fizz")
+        } else {
+            console.log(i);
+        }
     }
 }
 
