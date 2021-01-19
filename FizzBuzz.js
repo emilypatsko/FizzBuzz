989fb16ed67af8a9c39190aa19d9a077a998b123
Emily Patsko <Emily.Patsko@softwire.com>
2021-01-19 11:50:57 +0000

    Modified code to use and manipulate arrays rather than strings.

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,62 +1,65 @@
 function FizzBuzz() {
-    var i;
-    var output;
-    for (i = 1; i < 256; i++) {        
+    var i;    
+    for (i = 1; i < 256; i++) {  
+        var output = [];
+        
         if (i%11 == 0) {
             // If i is a multiple of 11, print "Bong" and nothing else
 
-            output = "Bong";
+            output.push("Bong");
         } else {
+            // If i is a multiple of 3, output Fizz 
+            // Divisible by 5, output Buzz
+            // Both, FizzBuzz
+
+            if (i%3 == 0 && i%5 == 0) {
+                output.push("Fizz");
+                output.push("Buzz");
+            } else if (i%3 == 0) {
+                output.push("Fizz");
+            } else if (i%5 == 0) {
+                output.push("Buzz");
+            } 
+
             // If i is a multiple of only 7 (not 3 or 5), output "Bang".
             // If it is also a multiple of 3 and/or 5 then append "Bang" to 
             // whatever output would have been produced 
 
-            if (i%3 == 0 && i%5 == 0) {
-                output = "FizzBuzz";
-            } else if (i%3 == 0) {
-                output = "Fizz";
-            } else if (i%5 == 0) {
-                output = "Buzz";
-            } else {
-                output = "";
-            }
-
             if (i%7 == 0) {
-                output = output + "Bang";     
+                output.push("Bang");     
             }     
         } 
 
         // If i is a multiple of 13, print Fezz instead of the number
         // If a multiple of the others as well, add Fezz before first instance of 'B'
         // or at the end if there are none.
+
         if (i%13 == 0) {
-            if (output == "") {
-                output = "Fezz";
-            } else if (output.includes("B")) {
-                var pos = output.indexOf("B");
-                output = [output.slice(0, pos), "Fezz", output.slice(pos)].join('');                
+            // Find first element which contains B
+            const ind = output.findIndex(element => element.includes("B"));
+
+            // If it exists, put Fezz before it. Otherwise, output is either empty or
+            // just doesn't contain words beginning with B. So append Fezz to the end
+            if (ind != null) {
+                output.splice(ind, 0, "Fezz");
             } else {
-                output = output + "Fezz";
+                output.push("Fezz");
             }
         }
 
         // If i is a multiple of 17, reverse the order of any Fizz, Buzz, Bong etc
         // which are printed
-        if (i%17 == 0 && output.length >= 8) {
-            var j;
-            var temp = "";
-            for (j = output.length - 4; j >= 0; j -= 4) {
-                temp = [temp, output.slice(j, j+4)].join('');                
-            }
-            output = temp;
+
+        if (i%17 == 0) {
+            output.reverse();
         }
 
         // If none of the above rules have applied, just print i!
-        if (output == "") {
-            output = i.toString();
+        if (output.length == 0) {
+            output.push(i);
         }
 
-        console.log(output);
+        console.log(output.join(''));
     }
 }
 
