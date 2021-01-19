480ea38eb8232cd2a41970378d1949c838e580f8
Emily Patsko <Emily.Patsko@softwire.com>
2021-01-19 10:04:48 +0000

    Print Fezz if i is a multiple of 13. If i is also a multiple or 3, 5 or 7, print Fezz in front of first instance of B. If there are none, append Fezz to the end of the output

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,10 +1,10 @@
 function FizzBuzz() {
     var i;
     var output;
-    for (i = 1; i < 106; i++) {        
+    for (i = 1; i < 201; i++) {        
         if (i%11 == 0) {
             // If i is a multiple of 11, print "Bong" and nothing else
-            
+
             output = "Bong";
         } else if (i%7 == 0) {
             // If i is a multiple of only 7 (not 3 or 5), output "Bang".
@@ -30,10 +30,27 @@ function FizzBuzz() {
                 output = "Fizz";
             } else if (i%5 == 0) {
                 output = "Buzz";
+            } 
+        }
+
+        // If i is a multiple of 13, print Fezz instead of the number
+        // If a multiple of the others as well, add Fezz before first instance of 'B'
+        // or at the end if there are none.
+        if (i%13 == 0) {
+            if (output == "") {
+                output = "Fezz";
+            } else if (output.includes("B")) {
+                var pos = output.indexOf("B");
+                output = [output.slice(0, pos), "Fezz", output.slice(pos)].join('');                
             } else {
-                output = i.toString();
+                output = output + "Fezz";
             }
         }
+
+        if (output == "") {
+            output = i.toString();
+        }
+
         console.log(output);
     }
 }
