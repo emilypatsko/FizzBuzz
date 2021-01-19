fa88a3e9172c0effadbb00b1c837bdb975b4b6dc
Emily Patsko <Emily.Patsko@softwire.com>
2021-01-19 09:22:56 +0000

    Added rule to output Bang for multiples of 7 (or append Bang to output if number is a multiple of 3 and/or 5). Also increased the amount of numbers printed to test this case for multiples of 3, 5 and 7

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,15 +1,34 @@
 function FizzBuzz() {
     var i;
-    for (i = 1; i < 101; i++) {
-        if (i%3 == 0 && i%5 == 0) {
-            console.log("FizzBuzz");
-        } else if (i%3 == 0) {
-            console.log("Fizz");
-        } else if (i%5 == 0) {
-            console.log("Buzz");
+    var output;
+    for (i = 1; i < 106; i++) {
+        // If i is a multiple of only 7 (not 3 or 5), output "Bang".
+        // If it is also a multiple of 3 and/or 5 then append "Bang" to 
+        // whatever output would have been produced 
+        if (i%7 == 0) {
+            if (i%3 == 0 && i%5 == 0) {
+                output = "FizzBuzz";
+            } else if (i%3 == 0) {
+                output = "Fizz";
+            } else if (i%5 == 0) {
+                output = "Buzz";
+            } else {
+                output = "";
+            }
+            output = output + "Bang";
+        // If i is not a multiple of 7, the usual FizzBuzz rules apply    
         } else {
-            console.log(i);
+            if (i%3 == 0 && i%5 == 0) {
+                output = "FizzBuzz";
+            } else if (i%3 == 0) {
+                output = "Fizz";
+            } else if (i%5 == 0) {
+                output = "Buzz";
+            } else {
+                output = i.toString();
+            }
         }
+        console.log(output);
     }
 }
 
